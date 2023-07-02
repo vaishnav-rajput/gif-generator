709cdb29a21c837b6bae7e752de8859f16425a3a
vaishnav <vaishnav.rajput108@gmail.com>
2023-07-03 01:59:06 +0530

    components seted up

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
+
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+// const tagMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`
+const randomMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+
 export default function App() {
   return (
     <div>
       <h1>
-
+        Gif Generator
       </h1>
       <div>
         <Random/>
---
+++ src/components/Random.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Random = () => {
+    return(
+        <div>
+            <h1>
+                
+            </h1>
+        </div>
+    )
+}
+
+export default Random;
