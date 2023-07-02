85b491a29fe854e68b78fba3783e432b1b1968a5
vaishnav <vaishnav.rajput108@gmail.com>
2023-07-03 04:11:15 +0530

    testing environment variables

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,11 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+// let API_KEY = {import.meta.env.VITE_REACT_APP_GIPHY_API_KEY};
 
 const Random = () => {
+    const [gif, setGif] = useState("");
+    const [loading, setLoading] = useState("");
+
+ 
+
+    async function fetchData(){
+        const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_SOMETHING}`
+ 
+        const output = await axios.get(url);
+        console.log(output)
+    }
+
+    useEffect(() => {
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_SOMETHING}`
+        console.log(url)
+    })
+
     return(
         <div>
             <h1>
-                
+                Random Gif
             </h1>
+            <img src={gif} />
+            <button>Generate</button>
         </div>
     )
 }
