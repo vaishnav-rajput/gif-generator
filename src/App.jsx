4537ae6ee3db430761d6d6cd5bd778b686a1ec40
vaishnav <vaishnav.rajput108@gmail.com>
2023-07-03 05:38:49 +0530

    tailwind css added

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ const randomMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
 
 export default function App() {
   return (
-    <div>
-      <h1>
+    <div className="w-full h-screen flex flex-col background relative items-center overflow-x-hidden">
+      <h1 className=" w-11/12 text-center mt-[40px] bg-white rounded-lg font-bold  px-10 py-2 text-3xl ">
         Gif Generator
       </h1>
-      <div>
+      <div className="flex flex-col w-full  items-center gap-y-10 mt-[30px]">
         <Random/>
         <Tag/> 
       </div>
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -31,16 +31,16 @@ const Tag = () => {
 
  
     return(
-        <div >
-            <h1>
+        <div className="w-1/2  bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+            <h1 className="text-2xl underline mt-[15px] uppercase font-bold">
                 Random {tag.toUpperCase()} Gif
             </h1>
             {
             loading ? (<Spinner/>) : (<img  src={gif}/>)
             }
 
-            <input onChange={(event) => setTag(event.target.value)} type="text" value={tag} />
-            <button onClick={() => (fetchData(tag))}>Generate</button>
+            <input className="w-9/12 text-lg py-2 rounded-lg mb-[2px] text-center" onChange={(event) => setTag(event.target.value)} type="text" value={tag} />
+            <button  className="w-9/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]" onClick={() => (fetchData(tag))}>Generate</button>
         </div>
     )
 }
