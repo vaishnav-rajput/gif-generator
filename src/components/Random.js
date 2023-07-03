bd10aaff53983bc19cdc65191febcfcb9ad7b27f
vaishnav <vaishnav.rajput108@gmail.com>
2023-07-03 05:36:08 +0530

    useGifs hook created

--- src/components/Random.js
+++ src/components/Random.js
@@ -2,39 +2,41 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 import "../index.css"
+import useGifs from "../hooks/useGifs";
 
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 
 const Random = () => {
-    const [gif, setGif] = useState("");
-    const [loading, setLoading] = useState(true);
+    // const [gif, setGif] = useState("");
+    // const [loading, setLoading] = useState(true);
 
  
 
-    async function fetchData(){
-        setLoading(true)
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    // async function fetchData(){
+    //     setLoading(true)
+    //     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
  
-        const output = await axios.get(url);
-        setGif(output.data.data.images.downsized_large.url);
-        setLoading(false)
-    }
+    //     const output = await axios.get(url);
+    //     setGif(output.data.data.images.downsized_large.url);
+    //     setLoading(false)
+    // }
 
-    useEffect(() => {
-        fetchData()
-    },[])
+    // useEffect(() => {
+    //     fetchData()
+    // },[])
 
+    const {gif, loading, fetchData} = useGifs();
     return(
-        <div>
-            <h1>
+        <div className="w-1/2  bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+            <h1 className="text-2xl underline mt-[15px] uppercase font-bold">
                 Random Gif
             </h1>
             {
             loading ? (<Spinner/>) : (<img  src={gif}/>)
             }
-            <button onClick={() => (fetchData())}>Generate</button>
+            <button onClick={() => (fetchData())} className="w-9/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]">Generate</button>
         </div>
     )
 }
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,36 +2,36 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Spinner from "./Spinner";
 import "../index.css"
+import useGifs from "../hooks/useGifs";
 
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 
 const Tag = () => {
-    const [gif, setGif] = useState("");
-    const [loading, setLoading] = useState(true);
+    // const [gif, setGif] = useState("");
+    // const [loading, setLoading] = useState(true);
     const [tag, setTag] = useState("car")
  
 
-    async function fetchData(tag){
-        setLoading(true)
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`
+    // async function fetchData(tag){
+    //     setLoading(true)
+    //     const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`
  
-        const output = await axios.get(url);
-        setGif(output.data.data.images.downsized_large.url);
-        setLoading(false)
-    }
+    //     const output = await axios.get(url);
+    //     setGif(output.data.data.images.downsized_large.url);
+    //     setLoading(false)
+    // }
 
-    useEffect(() => {
-        fetchData(tag)
-    },[])
+    // useEffect(() => {
+    //     fetchData(tag)
+    // },[])
 
-    function changeHandler(event){
-        setTag(event.target.value)
-    }
+    const {gif, loading, fetchData} = useGifs(tag);
 
+ 
     return(
-        <div>
+        <div >
             <h1>
                 Random {tag.toUpperCase()} Gif
             </h1>
@@ -39,7 +39,7 @@ const Tag = () => {
             loading ? (<Spinner/>) : (<img  src={gif}/>)
             }
 
-            <input onChange={changeHandler} type="text" value={tag} />
+            <input onChange={(event) => setTag(event.target.value)} type="text" value={tag} />
             <button onClick={() => (fetchData(tag))}>Generate</button>
         </div>
     )
